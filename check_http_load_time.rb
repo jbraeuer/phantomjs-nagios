f121d6512c398e392cd4b029af448fd323f4291f
Jonas Genannt <jonas@brachium-system.net>
2012-02-11 14:55:25 +0100

    fixed typos, added performance data, reworked time calcs

--- check_http_load_time.rb
+++ check_http_load_time.rb
@@ -2,15 +2,12 @@
 
 require 'json'
 require 'uri'
-require 'date'
 require 'time'
 require 'optparse'
 
-# Xvfb :1 -nolisten tcp -screen 1 1600x1200x16 &
-
 options = {}
 options[:phantomjs] = "/usr/bin/phantomjs --load-images=yes --load-plugins=yes --local-to-remote-url-access=yes --disk-cache=no"
-options[:snifferjs] = "/root/netsniff.js"
+options[:snifferjs] = "netsniff.js"
 options[:display]   = ":1"
 options[:warning]   = 1.0
 options[:critical]  = 2.0
@@ -21,14 +18,17 @@ OptionParser.new do |opts|
 	opts.on("-s", "--sniffer [STRING]", "path to phantomjs netsniff" ) do |s|
 		options[:snifferjs] = s
 	end
+	opts.on("-d", "--display [STRING]", "display for PhantomJS" ) do |d|
+		options[:display] = d
+	end
 	opts.on("-u", "--url [STRING]", "URL to query" ) do |u|
 		options[:url] = u
 	end
 	opts.on("-w", "--warning [FLOAT]", "Time when warning") do |w|
-		otions[:warning] = w
+		options[:warning] = w
 	end
 	opts.on("-c", "--critical [FLOAT]", "Time when critical") do |c|
-		otions[:critical] = c
+		options[:critical] = c
 	end
 end.parse!
 
@@ -46,34 +46,21 @@ rescue
 	exit 3
 end
 
-request_global_time_start = DateTime.parse(hash['log']['pages'][0]['startedDateTime'])
-request_global_time_end   = request_global_time_start
+request_global_time_start = Time.iso8601(hash['log']['pages'][0]['startedDateTime'])
+request_global_time_end   = Time.iso8601(hash['log']['pages'][0]['endedDateTime'])
 
-hash['log']['entries'].each do |entry|
-	request_time_start       = DateTime.parse(entry['startedDateTime'])
-	request_time_duration_ms = entry['time'].to_i / 1000.0
-	request_time_end         = request_time_start + request_time_duration_ms
-
-	if (request_time_end > request_global_time_end) 
-		request_global_time_end = request_time_end
-	end
-
-end
 website_load_time = '%.2f' % (request_global_time_end - request_global_time_start)
 
+performance_data = " | load_time=#{website_load_time.to_s}"
+
 if website_load_time.to_f > options[:critical].to_f
-	puts "Critical: #{website_url.to_s} load time: #{website_load_time.to_s}"
+	puts "Critical: #{website_url.to_s} load time: #{website_load_time.to_s}" + performance_data
 	exit 2
 elsif website_load_time.to_f > options[:warning].to_f
-	puts "Warning: #{website_url.to_s} load time: #{website_load_time.to_s}"
+	puts "Warning: #{website_url.to_s} load time: #{website_load_time.to_s}" + performance_data
 	exit 1
 else
-	puts "OK: #{website_url.to_s} load time: #{website_load_time.to_s}"
+	puts "OK: #{website_url.to_s} load time: #{website_load_time.to_s}" + performance_data
 	exit 0
 end
-
-
-
-
-
 
