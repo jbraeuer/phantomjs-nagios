91a442471d1f5e150398a025413484b0f7ad6eff
vagrant <jens.braeuer@commercetools.de>
2012-10-08 12:27:40 +0000

    Untabify.

--- check_http_load_time.rb
+++ check_http_load_time.rb
@@ -19,43 +19,43 @@ options[:debug] = false
 options[:xvfb] = false
 
 OptionParser.new do |opts|
-	opts.banner = "Usage: #{$0} [options]"
+        opts.banner = "Usage: #{$0} [options]"
 
-	opts.on("-s", "--sniffer [STRING]", "path to phantomjs netsniff" ) do |s|
-		options[:snifferjs] = s
-	end
-	opts.on("-u", "--url [STRING]", "URL to query" ) do |u|
-		options[:url] = u
-	end
-	opts.on("-w", "--warning [FLOAT]", "Time when warning") do |w|
-		options[:warning] = w
-	end
-	opts.on("-c", "--critical [FLOAT]", "Time when critical") do |c|
-		options[:critical] = c
-	end
-	opts.on("-m", "--min-elements [INT]", "Minimum number of elements to expect") do |m|
-		options[:min_elements] = m
-	end
-	opts.on("-p", "--phantomjs [PATH]", "Path to PhantomJS binary (default: #{options[:phantomjs_bin]})") do |p|
-		options[:phantomjs_bin] = p
-	end
-	opts.on("-n", "--netsniff [PATH]", "Path to netsniff.js script (default: #{options[:snifferjs]})") do |n|
-		options[:snifferjs] = n
-	end
-	opts.on("-e", "--html", "Add html tags to output url") do
-		options[:html] = true
-	end
-	opts.on("-d", "--debug", "Enable debug output") do
-		options[:debug] = true
-	end
-	opts.on("--xvfb-run", "Enable xfvb-run") do
-		options[:xvfb] = true
-	end
+        opts.on("-s", "--sniffer [STRING]", "path to phantomjs netsniff" ) do |s|
+                options[:snifferjs] = s
+        end
+        opts.on("-u", "--url [STRING]", "URL to query" ) do |u|
+                options[:url] = u
+        end
+        opts.on("-w", "--warning [FLOAT]", "Time when warning") do |w|
+                options[:warning] = w
+        end
+        opts.on("-c", "--critical [FLOAT]", "Time when critical") do |c|
+                options[:critical] = c
+        end
+        opts.on("-m", "--min-elements [INT]", "Minimum number of elements to expect") do |m|
+                options[:min_elements] = m
+        end
+        opts.on("-p", "--phantomjs [PATH]", "Path to PhantomJS binary (default: #{options[:phantomjs_bin]})") do |p|
+                options[:phantomjs_bin] = p
+        end
+        opts.on("-n", "--netsniff [PATH]", "Path to netsniff.js script (default: #{options[:snifferjs]})") do |n|
+                options[:snifferjs] = n
+        end
+        opts.on("-e", "--html", "Add html tags to output url") do
+                options[:html] = true
+        end
+        opts.on("-d", "--debug", "Enable debug output") do
+                options[:debug] = true
+        end
+        opts.on("--xvfb-run", "Enable xfvb-run") do
+                options[:xvfb] = true
+        end
 end.parse!
 
 unless File.executable?(options[:phantomjs_bin])
-	puts "Could not find PhantomJS binary (#{options[:phantomjs_bin]})"
-	exit 3
+        puts "Could not find PhantomJS binary (#{options[:phantomjs_bin]})"
+        exit 3
 end
 
 website_url = URI(options[:url])
@@ -64,20 +64,20 @@ website_load_time = 0.0
 # Run Phantom
 output = ""
 begin
-	Timeout::timeout(options[:critical].to_i) do
+        Timeout::timeout(options[:critical].to_i) do
                 cmd = ""
                 cmd += "/usr/bin/xvfb-run -a " if options[:xvfb]
                 cmd += options[:phantomjs_bin] + " " + options[:phantomjs_opts]  + " " + options[:snifferjs] + " " + website_url.to_s
                 warn "cmd is: #{cmd}" if options[:debug]
-		@pipe = IO.popen(cmd + " 2> /dev/null")
-		output = @pipe.read
-		Process.wait(@pipe.pid)
-	end
+                @pipe = IO.popen(cmd + " 2> /dev/null")
+                output = @pipe.read
+                Process.wait(@pipe.pid)
+        end
 rescue Timeout::Error => e
         puts "Critical: #{website_url.to_s}: Timeout after: #{options[:critical]}"
-	Process.kill(9, @pipe.pid)
-	Process.wait(@pipe.pid)
-	exit 2
+        Process.kill(9, @pipe.pid)
+        Process.wait(@pipe.pid)
+        exit 2
 end
 
 begin
@@ -88,10 +88,10 @@ begin
           # Remove from the output, so JSON can be parsed.
           output = output.split("\n").reject { |l| l =~ /^\d{4}-\d{2}-\d{2}/ }.join("\n")
         end
-	hash = JSON.parse(output)
+        hash = JSON.parse(output)
 rescue
-	puts "Unkown: Could not parse JSON from phantomjs"
-	exit 3
+        puts "Unkown: Could not parse JSON from phantomjs"
+        exit 3
 end
 
 request_global_time_start = Time.iso8601(hash['log']['pages'][0]['startedDateTime'])
@@ -106,20 +106,20 @@ performance_data = " | load_time=#{website_load_time_ms.to_s}ms size=#{request_s
 
 website_url_info = website_url.to_s
 if options[:html]
-	website_url_info = "<a href='" + website_url.to_s + "'>" + website_url.to_s + "</a>"
+        website_url_info = "<a href='" + website_url.to_s + "'>" + website_url.to_s + "</a>"
 end
 
 if website_load_time.to_f > options[:critical].to_f
-	puts "Critical: #{website_url_info} load time: #{website_load_time.to_s}" + performance_data
-	exit 2
+        puts "Critical: #{website_url_info} load time: #{website_load_time.to_s}" + performance_data
+        exit 2
 elsif website_load_time.to_f > options[:warning].to_f
-	puts "Warning: #{website_url_info} load time: #{website_load_time.to_s}" + performance_data
-	exit 1
+        puts "Warning: #{website_url_info} load time: #{website_load_time.to_s}" + performance_data
+        exit 1
 elsif request_elements.to_i < options[:min_elements].to_i
-	puts "Critical: #{website_url_info} number of elements: #{request_elements}" + performance_data
-	exit 2
+        puts "Critical: #{website_url_info} number of elements: #{request_elements}" + performance_data
+        exit 2
 else
-	puts "OK: #{website_url_info} load time: #{website_load_time.to_s}" + performance_data
-	exit 0
+        puts "OK: #{website_url_info} load time: #{website_load_time.to_s}" + performance_data
+        exit 0
 end
 
